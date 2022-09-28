a1f7f6f4bb9da9285bffbeab7b82903b7eac5220
UP210775 <up210775@alumnos.upa.edu.mx>
2022-09-28 10:43:25 -0500

    agregando pa

---
+++ U2/04_PIZZERIA.cpp
@@ -0,0 +1,20 @@
+/*
+date: 28/09/2022
+author: Oscar Alejandro Hernandez Gomez
+email:up210527@alumnos.upa.edu.mx
+description: program that ask for the type of pizza and the ingredients.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char pizza, ingredientes;
+
+    cout<<"Eliga la pizza de su gusto\n  vegetariana  no vegetariana";
+    cin>>pizza; 
+
+    if(pizza == "Vegetariana"
+    }
+
