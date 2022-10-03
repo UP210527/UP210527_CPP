ae4d0f010711325784ab77c5b5241884f7beebae
UP210775 <up210775@alumnos.upa.edu.mx>
2022-10-03 10:10:06 -0500

    agregando pa

--- U2/01_Renta.cpp
+++ U2/01_Renta.cpp
@@ -2,17 +2,17 @@
 date:22/09/2022
 author: Oscar Alejandro Hernandez Gomez
 email:Up210527@alumnos.upa.edu.mx
-descriion: programa que calcule la renta
+descriion: rent calculator program
 
-los tramos impositivos para la declaracion de la renta en un determinado país son los siguientes: 
+The tax brackets for the declaration of income in a certain country are the following:
 
-renta tipo impositivo 
-menos de $10,000    5%
-entre $10,000 y $20,000   15%
-entre $20,000 y $35,000   20%
-entre $35,000 y $60,000   30%
-mas de $60,000            45%
-Escribir un programa que pregunte al usuario su renta anual y muestre por pantalla el tipo imnpositivo que le corresponde
+income tax rate
+less than $10,000 5%
+between $10,000 and $20,000 15%
+between $20,000 and $35,000 20%
+between $35,000 and $60,000 30%
+more than $60,000 45%
+Write a program that asks the user for his annual income and shows on the screen the tax rate that corresponds to him
 */
 
 #include <iostream>
@@ -21,26 +21,26 @@ using namespace std;
 int main(){
     int renta;
     float impositivo;
-    cout<<"ingrese el valor de la renta anual: ";
+    cout<<"enter the value of the annual rent: ";
     cin>>renta;
 
     if(renta <=0){
-        cout<<"valor no aceptado";
+        cout<<"unaccepted value";
     }else if( renta<10000){
         impositivo = renta*0.05;
-        cout<<"se le aplica un 5 porciento de impuesto y su renta total es de: "<<impositivo+renta;
+        cout<<"a 5 percent tax is applied and his total income is: "<<impositivo+renta;
     }else if(renta >=10000 and renta<20000){
         impositivo= renta*0.15;
-        cout<<"se le aplica un 15 porciento de impuesto y su renta total es de: "<<impositivo+renta;
+        cout<<"a 15 percent tax is applied and his total income is: "<<impositivo+renta;
     }else if(renta>=20000 and renta<35000){
         impositivo=renta* 0.2;
-        cout<<"se le aplica un 20 por ciento de impuesto y su renta total es de: "<<impositivo+renta;
+        cout<<"20 percent tax is levied on him and his total income is: "<<impositivo+renta;
     }else if(renta>=35000 and renta<60000){
         impositivo=renta*0.3;
-        cout<<"se le aplica un 30 por ciento de impuesto y su renta total es de: "<<impositivo+renta;
+        cout<<"a 30 percent tax is applied to him and his total income is: "<<impositivo+renta;
     }else if(renta >=60000){
         impositivo= renta*0.45;
-        cout<<"se le aplica un 45 por ciento de impuesto y su renta total es de: "<<impositivo+renta;
+        cout<<"45 percent tax is levied on him and his total income is: "<<impositivo+renta;
     }
     return 0;
 }
